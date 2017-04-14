7de9e8d909880d3737a863793b0b9d50db13863e
mgiridhar <mgiridhar11@gmail.com>
2017-04-14 00:00:48 -0700

    finding rotating point in a rotated vector of strings

---
+++ array/binary_search_problems/rotated_string_array.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string myFunction (const vector<string> words)
+{
+    // write the body of your function here
+    if(!words.size())
+        return "";
+    if(words.size() == 1)
+        return words[0];
+    if(words.size() == 2)
+        return (words[0].compare(words[1])) ? words[0] : words[1];
+    unsigned long int l = 0, h = words.size() - 1, m;
+    while(l <= h) {
+        if(words[l].compare(words[h]) <= 0)
+            return words[l];
+        m = l + (h - l) / 2;
+        if(m > 0 && words[m-1].compare(words[m]) > 0)
+            return words[m];
+        if(m < words.size()-1 && words[m].compare(words[m+1]) > 0)
+            return words[m+1];
+        
+        if(words[l].compare(words[m]) < 0)
+            l = m+1;
+        else
+            h = m-1;
+    }
+    return "NOT FOUND";
+}
+
+int main ()
+{
+    // run your function through some test cases here
+    // remember: debugging is half the battle!
+    const vector<string> words {
+        "ptolemaic",
+        "retrograde",
+        "supplant",
+        "undulate",
+        "xenoepist",
+        "asymptote", // <-- rotates here!
+        "babka",
+        "banoffee",
+        "engender",
+        "karpatka",
+        "othellolagkage",
+    };
+    cout << myFunction (words);
+    return 0;
+}
